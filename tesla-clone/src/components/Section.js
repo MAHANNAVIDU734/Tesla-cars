03a6836a395c5e8d7f36ad06fd9ec32eaf45042b
Mahan <mahan.navidu@gmail.com>
2021-05-19 12:25:33 +0530

    react-redux

---
+++ tesla-clone/src/components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components";
+
+function Section() {
+  return (
+    <Wrap>
+      Section
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div'
+
+
+
+'
