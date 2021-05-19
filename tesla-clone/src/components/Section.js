b39e10402118462ab22937fcc4a0cfe880c35583
Mahan <mahan.navidu@gmail.com>
2021-05-19 16:19:42 +0530

    hamburger menu animation close out and open

--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from "styled-components";
+import Fade from 'react-reveal/Fade'; 
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
   return (
     <Wrap bgImage={backgroundImg}>
+    <Fade bottom>
 <ItemText>
 <h1>{ title }</h1>
 <p>{ description }</p>
 </ItemText>
+</Fade>
 <Buttons>
+<Fade bottom>
 <ButtonGroup>
 <LeftButton>
 {leftBtnText}
@@ -21,6 +25,7 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImg 
 }
 </RightButton>
 </ButtonGroup>
+</Fade>
 <DownArrow src="/images/down-arrow.svg" />
 </Buttons>
     </Wrap>
@@ -30,6 +35,7 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImg 
 export default Section;
 
 const Wrap = styled.div`
+   z-index: 10;
    width: 100vw;
    height: 100vh;
    background-size: cover;
@@ -45,6 +51,7 @@ const Wrap = styled.div`
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+  z-index: -1;
 `
 
 const ButtonGroup = styled.div`
